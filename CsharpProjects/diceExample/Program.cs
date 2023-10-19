41ffed52245939c745f7c13eab823fc3d2575d48
Leozinho11 <leoferraribarreto@hotmail.com>
2023-10-19 15:55:39 -0300

    Make some C# basic examples

---
+++ CsharpProjects/diceExample/Program.cs
@@ -0,0 +1,5 @@
+﻿Random dice = new Random();
+int roll = dice.Next(1, 7); // Semelhante ao Math.random do JS
+int noLimitRoll = dice.Next();
+Console.WriteLine(roll);
+Console.WriteLine(noLimitRoll);
